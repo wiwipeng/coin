9bd587e8a7fcdb735947d818ca4df22fd51a21db
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2022-12-12 20:26:40 +0800

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,73 +1,42 @@
-#@@ -1,20 +1,19 @@
 import streamlit as st
 import random
-import json
-import os
-def get_word(language: str, length: int) -> str:
-    with open(os.path.abspath(os.path.join('assets', 'language.json'))) as raw:
-        data = json.load(raw)[language]
-        word = random.choice(data)
-        while not 3 <= len(word) <= length or any(c in word for c in [' ', '-']):
-            word = random.choice(data)
+import dataclasses
 
-        return word
+from gamestate import persistent_game_state
 
+HI = 1000
 
-def init(language: str = 'English', length: int = 6, heart: int = 5, post_init=False):
-    if not post_init:
-        st.session_state.input = 0
-        st.session_state.win = 0
-    st.session_state.word = get_word(language, length)
-    st.session_state.lives = heart
-    st.session_state.guessed = []
-def restart():
-    init(st.session_state.language, st.session_state.length,
-         st.session_state.heart, post_init=True)
-    st.session_state.input += 1
-def main():
-    if 'word' not in st.session_state:
-        init()
-    st.title('🔠 Guess Word')
-    st.write('University project, still in development by **ccrsxx#8408**')
-    reset, won_game, lives, settings = st.columns([.45, .3,  1, 1])
-    reset.button('New game', on_click=restart)
-    with settings.expander('Settings'):
-        st.write('**Warning**: changing one of these settings will restart your game')
-        st.selectbox('Set language', [
-                     'English', 'Indonesia'], key='language', on_change=restart)
-        st.select_slider('Set hearts', list(range(1, 11)),
-                         5, key='heart', on_change=restart)
-        st.slider('Set length of the word', 3, 20,
-                  6, key='length', on_change=restart)
-    placeholder, debug = st.empty(), st.empty()
-    guess = placeholder.text_input(
-        'Guess a letter', key=st.session_state.input, max_chars=1).lower()
-    if not guess or not guess.isalpha():
-        debug.write('Please input letter')
-    elif guess in st.session_state.guessed:
-        debug.warning(f"You already guessed **{guess}**")
-    elif guess not in st.session_state.word:
-        debug.warning(f"The word has no **{guess}**")
-        st.session_state.lives -= 1
-        st.session_state.guessed.append(guess)
-    else:
-        debug.info('Good guess')
-        st.session_state.guessed.append(guess)
-    if st.session_state.lives == 0:
-        debug.error(
-            f"😓 **You lost**, the word was **{st.session_state.word}**")
-        placeholder.empty()
-    elif all(c in st.session_state.guessed for c in st.session_state.word):
-        debug.success(f"🎈 **YOU WIN**")
-        st.session_state.win += 1
-        placeholder.empty()
-    lives.button(
-        f'{("❤️" * st.session_state.lives) if st.session_state.lives else "💀 Lost"}')
-    won_game.button(f'🏆 {st.session_state.win}')
-    guess_box, letter_box = st.columns([1, .23])
-    guess_box.button(
-        ' - '.join([c if c in st.session_state.guessed else '_' for c in st.session_state.word]))
-    letter_box.button(
-        f'{" ".join(st.session_state.guessed) if st.session_state.guessed else "Letter History"}')
-if __name__ == '__main__':
-    main()
+@dataclasses.dataclass
+class GameState:
+    number: int
+    num_guesses: int = 0
+    game_number: int = 0
+    game_over: bool = False
+
+state = persistent_game_state(initial_state=GameState(random.randint(1, 1000)))
+
+if st.button("NEW GAME"):
+    state.number = random.randint(1, HI)
+    state.num_guesses = 0
+    state.game_number += 1
+    state.game_over = False
+
+if not state.game_over:
+    guess = st.text_input(f"guess a number between 1 and {HI}", key=state.game_number)
+
+    if guess:
+
+        try:
+            guess = int(guess)
+            state.num_guesses += 1
+
+            if guess < state.number:
+                st.write(f"{guess} is too low")
+            elif guess > state.number:
+                st.write(f"{guess} is too high")
+            else:
+                st.write(f"you win, it only took you {state.num_guesses} tries")
+                state.game_over = True
+
+        except ValueError:
+            st.write("please guess a *number*")
